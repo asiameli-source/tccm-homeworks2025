01e0e4ae330052cb607f61df02afd292874a7fd2
eabreudo <eloaabreu7782ab@gmail.com>
2026-01-05 16:37:00 +0100

    Add files via upload

---
+++ project1/homework_AEJ/html/4__prueba_8c.js
@@ -0,0 +1,5 @@
+var 4__prueba_8c =
+[
+    [ "get_eri", "4__prueba_8c.html#a607403243771f7cbb192d2cfe584d3ef", null ],
+    [ "main", "4__prueba_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4", null ]
+];
---
+++ project1/homework_AEJ/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,7 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "main.c", "main_8c.html", "main_8c" ],
+    [ "makefile", "makefile.html", null ],
+    [ "MP2_energy.c", "MP2__energy_8c.html", "MP2__energy_8c" ],
+    [ "MP2_energy.h", "MP2__energy_8h.html", "MP2__energy_8h" ]
+];
