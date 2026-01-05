f7c988aa69ce16dcde54fd5682f5e34067f2cf2b
eabreudo <eloaabreu7782ab@gmail.com>
2026-01-05 16:37:52 +0100

    Add files via upload

---
+++ project1/homework_AEJ/html/energy__mp2_8c.js
@@ -0,0 +1,5 @@
+var energy__mp2_8c =
+[
+    [ "G4", "energy__mp2_8c.html#a89bc149157a5b553f0207a904b7f9d4f", null ],
+    [ "main", "energy__mp2_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4", null ]
+];
