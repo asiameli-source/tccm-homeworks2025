739dac37ddb5d41988e952daed8ded6c753e78ca
yiyiyang01 <yiyi.yang@estudiante.uam.es>
2026-01-07 19:49:49 +0100

    Customize doxygen theme (purple)

--- project1/homework_AEJ/html/search/all_1.js
+++ project1/homework_AEJ/html/search/all_1.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['1_20—_20hartree–fock_20and_20mp2_20energies_20in_20c_20using_20trexio_0',['Project #1 — Hartree–Fock and MP2 Energies in C using TREXIO',['../md_README.html',1,'']]],
-  ['1_201_20installing_20hdf5_20library_1',['1.1. Installing HDF5 Library',['../md_INSTALL.html#autotoc_md3',1,'']]],
-  ['1_202_20installing_20trexio_20v2_205_200_2',['1.2. Installing TREXIO (v2.5.0)',['../md_INSTALL.html#autotoc_md4',1,'']]],
-  ['1_20compilation_20variables_3',['1. Compilation Variables',['../md_MAKEFILE.html#autotoc_md16',1,'']]],
-  ['1_20input_20file_4',['3.1 Input file',['../md_INSTALL.html#autotoc_md7',1,'']]],
-  ['1_20libtrexio_20so_20dynamic_20library_20not_20found_20linux_5',['4.1 libtrexio.so / dynamic library not found (Linux)',['../md_INSTALL.html#autotoc_md10',1,'']]],
-  ['1_20prerequisites_6',['1. Prerequisites',['../md_INSTALL.html#autotoc_md2',1,'']]]
+  ['1_201_20installing_20hdf5_20library_0',['1.1. Installing HDF5 Library',['../md_INSTALL.html#autotoc_md3',1,'']]],
+  ['1_202_20installing_20trexio_20v2_205_200_1',['1.2. Installing TREXIO (v2.5.0)',['../md_INSTALL.html#autotoc_md4',1,'']]],
+  ['1_20compilation_20variables_2',['1. Compilation Variables',['../md_MAKEFILE.html#autotoc_md16',1,'']]],
+  ['1_20input_20file_3',['3.1 Input file',['../md_INSTALL.html#autotoc_md7',1,'']]],
+  ['1_20libtrexio_20so_20dynamic_20library_20not_20found_20linux_4',['4.1 libtrexio.so / dynamic library not found (Linux)',['../md_INSTALL.html#autotoc_md10',1,'']]],
+  ['1_20prerequisites_5',['1. Prerequisites',['../md_INSTALL.html#autotoc_md2',1,'']]],
+  ['1_20—_20hartree–fock_20and_20mp2_20energies_20in_20c_20using_20trexio_6',['Project #1 — Hartree–Fock and MP2 Energies in C using TREXIO',['../md_README.html',1,'']]]
 ];
